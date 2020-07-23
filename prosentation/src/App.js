88ab82b0ceaf46a3173eb4a2b375688145f012a6
joi0104 <joi0104@naver.com>
2020-07-23 23:42:04 +0900

    fix(login): fix import error

--- prosentation/src/App.js
+++ prosentation/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter,Route,Switch } from "react-router-dom"
 
 import Main from './pages/Main'
+import Login from './pages/Login'
 
 function App() {
   return (
