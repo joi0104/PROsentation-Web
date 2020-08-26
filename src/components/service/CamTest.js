54b2864282157a86729f79c42a1c405eb189992a
joi0104 <joi0104@naver.com>
2020-08-27 01:29:25 +0900

    feat(camTest): #6. 카메라테스트 작업

--- src/components/service/CamTest.js
+++ src/components/service/CamTest.js
@@ -1,23 +1,61 @@
-import React from 'react'
-import classNames from 'classnames/bind'
+import React, { useEffect } from "react";
+import classNames from "classnames/bind";
 
-import style from 'components/service/CamTest.scss'
+import style from "components/service/CamTest.scss";
 
-const cx = classNames.bind(style)
+const cx = classNames.bind(style);
 
 const CamTest = ({ setIsCamTest }) => {
+  const constraints = (window.constraints = {
+    audio: false,
+    video: true,
+  })
 
-    const onClick = () => {
-        setIsCamTest(true)
+  useEffect(() => {
+    navigator.mediaDevices.getUserMedia(constraints)
+    .then(stream => {
+      handleSuccess(stream)
+    })
+    .catch(error => {
+      handleError(error)
+    });
+  }, )
+
+  function handleSuccess(stream) {
+    const video = document.querySelector('video')
+    video.srcObject = stream
+    video.onloadedmetadata = function(e) {
+      video.play()
+      handleMsg('연결 성공!')
+    };
+  }
+  
+  function handleError(error) {
+    if (error.name === 'ConstraintNotSatisfiedError') {
+      const v = constraints.video
+      handleMsg(`${v.width.exact}x${v.height.exact}크기의 카메라를 지원하지 않아요 ㅠㅠ`)
+    } else if (error.name === 'PermissionDeniedError') {
+      handleMsg('카메라 요청을 허가해주세요!');
     }
-    
-    return (
-        <div className={cx('CamTest')}>
-            카메라 테스트중..
-            <button onClick={onClick}>다음</button>
-            <video id="localVideo" />
-        </div>
-    );
+    handleMsg(`에러 발생!${error.name}`, error);
+  }
+
+  function handleMsg(msg) {
+    const msgElement = document.querySelector('#msg')
+    msgElement.innerHTML = `<p>${msg}</p>`
+  }
+
+  function goNext() {
+    setIsCamTest(true)
+  }
+
+  return (
+    <div className={cx("CamTest")}>
+    <video autoPlay playsInline></video>
+    <p id="msg">카메라 테스트를 시작해주세요.</p>
+    <button onClick={goNext}>다음</button>
+    </div>
+  );
 };
 
 export default CamTest;
