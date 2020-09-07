5c200784cf468f3591d2c0a820e4d71cce4031b2
joi0104 <jjjjoi0104@gmail.com>
2020-09-08 00:39:41 +0900

    refactor: refactoring camTest, micTest, pptTest

--- src/components/service/CamTest.js
+++ src/components/service/CamTest.js
@@ -9,6 +9,7 @@ const cx = classNames.bind(style);
 const CamTest = ({ setIsCamTest }) => {
   const [testOK, setTestOK] = useState(false);
   const video = useRef();
+  const msg = useRef();
   const constraints = (window.constraints = {
     audio: false,
     video: true,
@@ -20,49 +21,48 @@ const CamTest = ({ setIsCamTest }) => {
       .then((stream) => {
         handleSuccess(stream);
       })
-      .catch((error) => {
-        handleError(error);
+      .catch((err) => {
+        handleError(err);
       });
   });
 
-  function handleSuccess(stream) {
+  const handleSuccess = (stream) => {
     video.current.srcObject = stream;
-    video.current.onloadedmetadata = function (e) {
+    video.current.onloadedmetadata = (e) => {
       video.current.play();
       setTestOK(true);
       handleMsg("연결 성공!");
     };
-  }
+  };
 
-  function handleError(error) {
-    if (error.name === "ConstraintNotSatisfiedError") {
+  const handleError = (err) => {
+    if (err.name === "ConstraintNotSatisfiedError") {
       handleMsg(
         `${constraints.video.width.exact}x${constraints.video.height.exact}크기의 카메라를 지원하지 않아요 ㅠㅠ`
       );
-    } else if (error.name === "PermissionDeniedError") {
+    } else if (err.name === "PermissionDeniedError") {
       handleMsg("카메라 요청을 허가해주세요!");
     } else {
-      handleMsg(`에러 발생!${error.name}`);
+      handleMsg(`에러 발생!${err.name}`);
     }
-  }
+  };
 
-  function handleMsg(msg) {
-    const msgElement = document.querySelector("#msg");
-    msgElement.innerHTML = `<p>${msg}</p>`;
-  }
+  const handleMsg = (msgStr) => {
+    msg.current.text = msgStr;
+  };
 
-  function goNext() {
+  const goNext = () => {
     if (testOK) {
       setIsCamTest(true);
     } else {
       alert("카메라 테스트를 완료해주세요!");
     }
-  }
+  };
 
   return (
     <div className={cx("CamTest")}>
       <video ref={video} autoPlay playsInline></video>
-      <p id="msg">카메라 테스트를 시작해주세요.</p>
+      <p ref={msg}>카메라 테스트를 시작해주세요.</p>
       <button onClick={goNext}>다음</button>
     </div>
   );
