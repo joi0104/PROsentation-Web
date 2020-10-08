d86c442e8ac05731fa7a9a5c4f770aab69423bf6
joi0104 <jjjjoi0104@gmail.com>
2020-10-08 16:27:49 +0900

    refator: Recoding 오타 수정

---
+++ src/components/service/recording/Recording.js
@@ -0,0 +1,24 @@
+import React, { useEffect } from 'react'
+import classNames from 'classnames/bind'
+
+import style from './Recording.scss'
+import RecordingVideo from 'components/service/recording/RecordingVideo.js'
+import PPTView from 'components/service/recording/PPTView.js'
+
+const cx = classNames.bind(style)
+
+const Recording = ({ setRecordingOK, serviceId, PPT }) => {
+  const goNext = () => {
+    setRecordingOK(true)
+  }
+
+  return (
+    <div className={cx('Recording')}>
+      <PPTView PPT={PPT} />
+      <RecordingVideo />
+      <button onClick={goNext}>완료</button>
+    </div>
+  )
+}
+
+export default Recording
