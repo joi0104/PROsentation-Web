72c61094d0d036dab930ce3725745b3def920942
joi0104 <jjjjoi0104@gmail.com>
2020-10-18 23:57:52 +0900

    refactor: #13. 로그인 퍼블리싱 하기

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -1,27 +1,34 @@
 import React from 'react'
 import classNames from 'classnames/bind'
-import { useHistory } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 import style from 'pages/Signin.scss'
 import SigninForm from 'components/signin/SigninForm.js'
 import Button2 from 'elements/Button2.js'
-import FindLink from 'elements/FindLink.js'
+import Line from 'elements/Line.js'
 
 const cx = classNames.bind(style)
 
 const Signin = ({ setHasCookie }) => {
-  let history = useHistory()
-
-  const onClick = () => {
-    history.push('/signup')
-  }
-
   return (
     <div className={cx('Signin')}>
       <div className={cx('Signin-wrapper')}>
-        <SigninForm setHasCookie={setHasCookie} />
-        <FindLink />
-        <Button2 onClick={onClick}>회원가입</Button2>
+        <div className={cx('Signin-content')}>
+          <SigninForm setHasCookie={setHasCookie} />
+          <div className={cx('Find-links')}>
+            <Link className={cx('Find-link')} to="/find-password">
+              비밀번호 찾기
+            </Link>
+            <Link className={cx('Find-link')}>|</Link>
+            <Link className={cx('Find-link')} to="/signin">
+              로그인 하기
+            </Link>
+          </div>
+          <Line />
+          <Link to="/signup" style={{ textDecoration: 'none' }}>
+            <Button2>간편가입하기</Button2>
+          </Link>
+        </div>
       </div>
     </div>
   )
