1dd680734cf17d25f1d47556e5435c65e0f2ed85
joi0104 <jjjjoi0104@gmail.com>
2020-11-02 18:23:05 +0900

    feat: 로그인시 navbar 변경하기

--- src/components/navbar/Profile.js
+++ src/components/navbar/Profile.js
@@ -1,15 +1,18 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
 import classNames from 'classnames/bind'
 import { useHistory } from 'react-router-dom'
 
 import style from 'components/navbar/Profile.scss'
 import profileIcon from 'assets/images/profile.png'
+import userIcon from 'assets/icons/icon-user.png'
 import ProfileDropdown from 'components/navbar/ProfileDropdown.js'
+import UserContext from 'contexts/user.js'
 
 const cx = classNames.bind(style)
 
 const Profile = ({ hasCookie, setHasCookie }) => {
   const [showDropdown, setShowDropdown] = useState(false)
+  const { state } = useContext(UserContext)
   let history = useHistory()
 
   const onClick = () => {
@@ -22,7 +25,14 @@ const Profile = ({ hasCookie, setHasCookie }) => {
 
   return (
     <div className={cx('Profile')}>
-      <img src={profileIcon} alt="profileIcon" onClick={onClick} />
+      {hasCookie ? (
+        <>
+          <p>반가워요! {state.username}님</p>
+          <img src={userIcon} alt="profileIcon" onClick={onClick} />
+        </>
+      ) : (
+        <img src={profileIcon} alt="profileIcon" onClick={onClick} />
+      )}
       {showDropdown ? (
         <ProfileDropdown
           setShowDropdown={setShowDropdown}
